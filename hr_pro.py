695e8bf9d9c5be381e962d49fb33a8413d95942c
hanodims <hanodims@hotmail.com>
2020-08-20 14:07:20 +0300

    updated

--- hr_pro.py
+++ hr_pro.py
@@ -4,21 +4,17 @@ class Employee:
     def __init__(self, name, age, salary,employment_year):
         self.name = name
         self.age = age
-        self.salary = salary
-        self.employment_year = employment_year
-        self.emp = []
-        self.emp.append({
-            'name' : self.name ,
-            'age' : self.age,
-            'salary' : self.salary,
-            'employment_year' : self.employment_year 
-        })
+        self.salary = int(salary)
+        self.employment_year = self.get_working_years(int(employment_year))
+        
     
-    def get_working_years(self):
-        return (datetime.today() - self.employment_year)
+    def get_working_years(self,employment_year):
+        return (datetime.today().year - employment_year )
     
     def __str__(self):
-        return super().__str__(self.emp)
+        employee = self.name
+        return  f"\nName: {self.name}, Age: {self.age}, Salary: {self.salary}, Working year: {self.employment_year}"
+        
 
 
 
@@ -26,29 +22,18 @@ class Manager(Employee):
     
     def __init__(self, name, age, salary, employment_year,bonus_percentage):
         super().__init__(name, age, salary, employment_year)
-        self.bonus_percentage = bonus_percentage
-        self.man = []
-        self.man.append([{
-            'name' : self.name,
-            'age' : self.age,
-            'salary' : self.salary,
-            'employment_year' : self.employment_year,
-            'bonus_percentage' : self.bonus_percentage
-        }])
+        self.bonus_percentage = self.get_bonus(float(bonus_percentage))
     
-    def get_bonus(self):
-        return self.bonus_percentage * self.salary
+    def get_bonus(self,bonus_percentage):
+        return bonus_percentage * self.salary
     def __str__(self):
-        return super().__str__(self.man)
+        return super().__str__() + f", Bonus: {self.bonus_percentage} "
 
         
 def main():
-    name = 'e'
-    age = 25
-    salary = 3000
-    emp_year = 2018
-    bonus_percentage = .3
-    print('Welcome to HR Pro 2019')
+    emp = []
+    man = []
+    print('\nWelcome to HR Pro 2019')
     print('Options:')
     print("""
     1. Show Employees
@@ -57,21 +42,43 @@ def main():
     4. Add A Manager
     5. Exit """)
     choice = input('\nWhat would you like to do? ')
-    print('-'*15)
-    if choice == '1':
-        lst = Employee(name,age,salary,emp_year)
-        print(lst.emp)
-    elif choice == '2':
-        lst = Manager(name,age,salary,emp_year,bonus_percentage)
-        print(lst.man)
-    elif choice == '3':
-        name = input('Name: ')
-        age = input('Age: ')
-        salary = input('Salary: ')
-        emp_year = input('Employement year: ')
-        a = Employee(name,age,salary,emp_year)
-        print(a.emp)
-        #a.emp.append(name,age,salary,emp_year)
+    print('-'*20)
+    while choice != '5':
+        
+        print()
+        if choice == '1':
+            print("Employees")
+            for i in emp:
+                print(i)
+        if choice == '2':
+            print('Managers')
+            for i in man:
+                print(i)
+        if choice == '3':
+            name = input('Name: ')
+            age = input('Age: ')
+            salary = input('Salary: ')
+            emp_year = input('Employement year: ')
+            print()
+            a = Employee(name,age,salary,emp_year)
+            emp.append(a)
+            print("\nEmployee added succesfully")
+            print('-'*15)
+            
+        if choice == '4':
+            name = input('Name: ')
+            age = input('Age: ')
+            salary = input('Salary: ')
+            emp_year = input('Employement year: ')
+            bonus_percent = input('Bonus percentage: ')
+            print()
+            a = Manager(name,age,salary,emp_year,bonus_percent)
+            man.append(a)
+            print("\nManager added succesfully")
+            print('-'*15)
+
+        choice = input('\nWhat would you like to do? ')
+        
 
         #Not Finished Yet
        
